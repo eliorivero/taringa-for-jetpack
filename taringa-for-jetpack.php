c6478a317d710ff009b0ba63b2c293a0c500b914
Elio Rivero <eliorivero@gmail.com>
2016-08-27 12:53:02 -0300

    Start adding sharing service

--- taringa-for-jetpack.php
+++ taringa-for-jetpack.php
@@ -12,14 +12,48 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
+define( 'JPFT__PLUGIN_DIR',  plugin_dir_path( __FILE__ ) );
+define( 'JPFT__PLUGIN_FILE', __FILE__ );
+
+// Localize
 add_action( 'plugins_loaded', 'jpft_localization' );
 
+// Insert our CSS and JS
+add_action( 'load-settings_page_sharing', 'jpft_sharing_head' );
+
+// Add button
+add_filter( 'sharing_services', 'jpft_add_sharing' );
+
 /**
- * Initialize localization routines
+ * Add button to list of sharing services.
+ *
+ * @since 0.0.7
+ */
+function jpft_add_sharing( $services ) {
+	include_once JPFT__PLUGIN_DIR . 'class.share-taringa.php';
+
+	if ( ! array_key_exists( 'taringa', $services ) ) {
+		$services['taringa'] = 'JPFT_Share_Taringa';
+	}
+
+	return $services;
+}
+
+/**
+ * Add styles and scripts.
+ *
+ * @since 0.0.7
+ */
+function jpft_sharing_head() {
+	wp_enqueue_style( 'share-taringa', plugins_url( 'css/share-taringa.css', __FILE__ ), array(), JETPACK__VERSION );
+}
+
+/**
+ * Initialize localization routines.
  *
  * @since 0.0.7
  * @access public
  */
 function jpft_localization() {
-	load_plugin_textdomain( 'jetpack', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	load_plugin_textdomain( 'jetpack', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
 }
